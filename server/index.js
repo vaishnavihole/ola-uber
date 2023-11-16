4fbc8ba4363f522f350d97b61bcd837eb18ca3ba
vaishnavihole <vaishnavihole1@gmail.com>
2023-11-16 08:56:09 +0530

    login api added

--- server/index.js
+++ server/index.js
@@ -14,12 +14,43 @@ async function connectMongoDB() {
 }
 connectMongoDB();
 
+// health api
 app.get("/health", (req, res) => {
     res.json({
         success: true,
         message: "All Good🏆",
     });
 });
+
+// POST / Login
+app.post("/login", async (req, res) => {
+    const { email, password } = req.body
+
+    if (!email || !password) {
+        return res.json({
+            success: false,
+            message: 'please provide email and password'
+        })
+    }
+    const user = await User.findOne({
+        email: email,
+        password: password
+    })
+
+    if (user) {
+        return res.json({
+            success: true,
+            data: user,
+            message: "Login successful"
+        });
+    }
+    else {
+        return res.json({
+            success: false,
+            message: "Invalid credentials"
+        })
+    }
+})
 
 
 const PORT = process.env.PORT || 5000;
