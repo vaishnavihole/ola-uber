23e7a8a4b02ef822c89b96b6225644e22e296148
vaishnavihole <vaishnavihole1@gmail.com>
2023-11-18 17:18:15 +0530

    login form added

--- client/src/views/Login/Login.js
+++ client/src/views/Login/Login.js
@@ -1,9 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './Login.css';
 
-function Login() {
+const Login = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle login logic here
+    console.log('Login submitted:', formData);
+  };
+
   return (
-    <div>Login</div>
-  )
+    <div className="login-container">
+      <form onSubmit={handleSubmit} className="login-form">
+        <h2 className="form-title">Login</h2>
+
+        <label className="label">
+          Email:
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            className="input-field"
+            required
+          />
+        </label>
+
+        <label className="label">
+          Password:
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            className="input-field"
+            required
+          />
+        </label>
+
+        <button type="submit" className="button">
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
 }
 
-export default Login
+export default App;
