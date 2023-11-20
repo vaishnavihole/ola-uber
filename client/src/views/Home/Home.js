dedd1ca82948288361c5b3d9b9836235dd816163
vaishnavihole <vaishnavihole1@gmail.com>
2023-11-20 13:27:35 +0530

    home page added

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import './Home.css'
+import Navbar from '../../components/Navbar/Navbar'
+import DelivaryBoy from './delivary-boy.png'
+import Footer from '../../components/Footer/Footer'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <Navbar />
+      <div className='row'>
+        <div className='col-md-6'>
+          <div className='header-img-container'><img className='header-image' src={DelivaryBoy} /></div>
+        </div>
+        <div className='col-md-6'>
+          <div className='tagline-container'>
+          <h1 className='tagline'>We make life of elderly people super easy by providing various services</h1> 
+          </div>
+        </div>
+        
+         <div className='feature-card'>
+              {/* <img src={retailers} className="img-manufacturer" alt="retailers" /> */}
+              <h3 className='title'>Retailers</h3>
+              {/* <Link to='/retailer'><img src={rightArrow} className='right-arrow' alt="arrow" /></Link> */}
+            </div>
+      </div>
+      <Footer />
+      </div>
   )
 }
 
